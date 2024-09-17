9d8b6217c9781a9645e52e5b3c283a3d18b1e5fc
N22R <nazarmelnyk.nzr@gmail.com>
2024-09-17 15:58:33 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,18 @@
+# Лабораторна робота з дисципліни "Алгоритмізація та програмування"
+# Виконав: Мельник Назарій Володимирович (ІР-14)
+# Лабораторна робота №1 (Варіант 13)
+drink = 'Вода'
+print(drink)
+is_holiday = True
+is_workday = False
+print(is_holiday)
+print(is_workday)
+is_sunny = True
+is_cloudy = False
+print(is_sunny or is_cloudy)
+import math
+x = 5.168
+z = 1.569
+y = 3.022
+ma = (x+1)**2+(2*(y+z))/(x+y-z**2)+13*math.log(5,(x*y+z))
+print(ma)
